3014ab643663092da5faba163200518b52d858d2
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2019-10-22 13:08:25 -0400

    Draft form for Feedback

--- assets/js/formSlider.js
+++ assets/js/formSlider.js
@@ -7,4 +7,22 @@ $( document ).ready(function() {
       $('.toggle-form, .formwrap, .toggle-bg').removeClass('active');
       $('.icon-close').removeClass('open');
   });
+  $('#feedback').on('submit',(function(e) {
+    var formData = $("#feedback").serializeArray();
+    var feedbackData = {
+      "feedback":[{"comment":formData[0].value,"email":formData[1].value}]       
+    }
+    $.ajax({
+      type: "POST",
+      url: "/ghost/api/v2/content/beta/feedback?key=6d95c73663e7a0cef99607a8a0",
+      data: JSON.stringify(feedbackData),
+      success: function(){},
+      dataType: "json",
+      contentType : "application/json"
+    });
+    $('.toggle-form, .formwrap, .toggle-bg').removeClass('active');
+    $('.icon-close').removeClass('open');
+
+
+  }))
 });
