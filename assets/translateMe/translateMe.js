ccd7f00bb586baf5d41422f4e57b2b6c0c35f8cc
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2019-11-13 11:03:50 -0500

    Themeing beta banner

--- assets/translateMe/translateMe.js
+++ assets/translateMe/translateMe.js
@@ -216,7 +216,8 @@ const translateMe = {
         "Subscribe to our newsletter": "Subscribe to our newsletter",
         "http://eepurl.com/gdiUTH": "http://eepurl.com/gdiUTH",
         "https://csps-efpc.gc.ca/About_us/Business_lines/digitalacademy-eng.aspx": "https://csps-efpc.gc.ca/About_us/Business_lines/digitalacademy-eng.aspx",
-        "Busrides is in the beta phase of development. Regular site updates are made to enhance your experience!": "Busrides is in the beta phase of development. Regular site updates are made to enhance your experience!",
+        "Busrides is in the beta phase of development.": "Busrides is in the beta phase of development.",
+        "Regular site updates are made to enhance your experience!": "Regular site updates are made to enhance your experience!",
         "mailto:csps.digitalacademy-academiedunumerique.efpc@canada.ca?Subject=Busrides%20Feedback": "mailto:csps.digitalacademy-academiedunumerique.efpc@canada.ca?Subject=Busrides%20Feedback",
         "Provide feedback": "Provide feedback"
     },
@@ -266,7 +267,8 @@ const translateMe = {
         "Subscribe to our newsletter": "Abonnez-vous à notre infolettre",
         "http://eepurl.com/gdiUTH": "http://eepurl.com/gdiUTz",
         "https://csps-efpc.gc.ca/About_us/Business_lines/digitalacademy-eng.aspx": "https://csps-efpc.gc.ca/About_us/Business_lines/digitalacademy-fra.aspx",
-        "Busrides is in the beta phase of development. Regular site updates are made to enhance your experience!": "Trajet en bus est en phase de développement bêta. Des mises à jour régulières sont faites pour améliorer votre expérience!",
+        "Busrides is in the beta phase of development." : "Trajet en bus est en phase de développement bêta.",
+        "Regular site updates are made to enhance your experience!": "Des mises à jour régulières sont faites pour améliorer votre expérience!",
         "mailto:csps.digitalacademy-academiedunumerique.efpc@canada.ca?Subject=Busrides%20Feedback": "mailto:csps.digitalacademy-academiedunumerique.efpc@canada.ca?Subject=Busrides%20Rétroaction",
         "Provide feedback": "Fournir de la rétroaction"
     }  
