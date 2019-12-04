f905cbb71090753fa68d81d1aea4865ed384556e
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2019-12-04 10:43:45 -0500

    Fixed form issues

--- assets/js/formSlider.js
+++ assets/js/formSlider.js
@@ -16,9 +16,10 @@ $(document).ready(function () {
         var feedbackData = {
             feedback: [{ comment: formData[0].value, email: formData[1].value }]
         };
+
         $.ajax({
             type: 'POST',
-            url: '/ghost/api/v2/content/beta/feedback?key=' + process.env('GHOST_FEEDBACK_API_KEY'),
+            url: '/ghost/api/v2/content/beta/feedback?key=6d95c73663e7a0cef99607a8a0',
             data: JSON.stringify(feedbackData),
             success: function () {},
             dataType: 'json',
