c2ac47e4ffb0978933179d2fa3c2f81373a232ae
Jordan Morrison <32398038+Jordan-Morrison@users.noreply.github.com>
2020-02-21 13:40:18 -0500

    Dynamic Hero Component
    Updated the hero component to be more dynamic. It can now be re-used across all the pages and will render out any children passed inside.

--- components/templates/Episode.js
+++ components/templates/Episode.js
@@ -1,4 +1,5 @@
 import Header from '../organisms/Header';
+import Hero from '../organisms/Hero';
 import PostContent from '../organisms/PostContent';
 import Footer from '../organisms/Footer';
 
@@ -9,6 +10,7 @@ export default function Episode(props) {
     return (
         <div>
             <Header t={props.t} settings={props.settings}/>
+            <Hero backgroundImage={props.post.feature_image}/>
             <h1>{props.post.title}</h1>
             <PostContent html={props.post.html}/>
             <Footer t={props.t}/>
--- components/templates/Home.js
+++ components/templates/Home.js
@@ -2,15 +2,19 @@ import Header from '../organisms/Header';
 import Hero from '../organisms/Hero';
 import EpisodeList from '../organisms/EpisodeList';
 import Footer from '../organisms/Footer';
+import styles from '../stylesheets/Home.module.css';
 
 export default function Home(props) {
 
-    console.log(props.posts);
+    console.log(props.settings);
 
     return (
         <div>
             <Header t={props.t} settings={props.settings}/>
-            <Hero t={props.t}/>
+            <Hero backgroundImage={props.settings.cover_image}>
+                <img className={styles.logo} src={props.t.getLogo}/>
+                <p className={styles.description}>{props.t.getDescription}</p>
+            </Hero>
             <EpisodeList t={props.t} pages={props.pages} posts={props.posts} tags={props.tags}/>
             <Footer t={props.t}/>
         </div>
