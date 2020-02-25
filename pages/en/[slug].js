cbda7e18388fc22bad1f05de377dc7b652ae02fb
Jordan Morrison <32398038+Jordan-Morrison@users.noreply.github.com>
2020-02-25 14:26:58 -0500

    Episode Page Error Handling
    Non-existing slugs in the url now route you to the 404 page

--- pages/en/[slug].js
+++ pages/en/[slug].js
@@ -1,16 +1,28 @@
 import { getPost, getRecommendedPosts, getTags, getSettings } from '../../Ghost-API/contentAPI';
 import Episode from '../../components/templates/Episode';
 import dictionary from '../../locales/en';
+import ErrorPage from '../_error';
 
 export default function Post(props) {
+
+    if (props.error){
+        return <ErrorPage errorCode={404} />
+    }
 
     return (
         <Episode t={dictionary} post={props.post} tags={props.tags} settings={props.settings} recommendedPosts={props.recommendedPosts}/>
     );
 }
 
-Post.getInitialProps = async function({query}) {
+Post.getInitialProps = async function({query, res}) {
+
     const post = await getPost(query.slug);
+
+    if (!post){
+        res.statusCode = 404
+        return {error: true};
+    }
+
     const tags = await getTags(dictionary.getTopicSlugs);
     const settings = await getSettings();
     const recommendedPosts = await getRecommendedPosts(dictionary.getGhostLocaleTag, post);
