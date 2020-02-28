a42e4a4802241a4b5e6c14330b6f8eca9a513acb
Jordan Morrison <32398038+Jordan-Morrison@users.noreply.github.com>
2020-02-28 10:11:10 -0500

    Mobile Header
    Made a working mobile version of the header. I think this could be made better and should be discussed on how we can improve these components

--- components/organisms/Header.js
+++ components/organisms/Header.js
@@ -1,8 +1,12 @@
 import IcomoonReact from "icomoon-react";
 import iconSet from "../icons/selection.json";
+import Link from 'next/link';
 import styles from '../stylesheets/Header.module.css';
+import { useState } from "react";
 
 export default function Header(props) {
+
+    const [isMenuOpen, openMenu] = useState(false);
 
     function getHomeUrl() {
         return props.t.getLocale === "en" ? "/" : "/fr";
@@ -19,22 +23,29 @@ export default function Header(props) {
         return props.settings.navigation.filter(navItem => navItem.url.includes("/fr/"));
     }
 
+    function toggleMenu() {
+        openMenu(!isMenuOpen);
+    }
+
     return (
         <div className={styles.header}>
             <div className={styles.headerContent}>
+                <div className={styles.menuIcon} onClick={toggleMenu}>
+                    <IcomoonReact iconSet={iconSet} size={18} icon="menu"/>
+                </div>
+                <Link href={getHomeUrl()}>
+                    <img className={styles.logo} src={props.settings.logo} alt={props.settings.title}/>
+                </Link>
                 <ul className={styles.navItems}>
-                    <li>
-                        <a href={getHomeUrl()}>
-                            <img className={styles.logo} src={props.settings.logo} alt={props.settings.title}/>
-                        </a>
-                    </li>
                     {getNavItems().map(navItem => (
                         <li key={navItem.url}>
-                            <a className={styles.navItem} href={navItem.url}>{navItem.label}</a>
+                            <Link href={navItem.url}>
+                                <a className={styles.navItem}>{navItem.label}</a>
+                            </Link>
                         </li>
                     ))}
                     <li>
-                        <a className={styles.navItem} href={getHomeUrl()}>{props.t["Topics"]}</a>
+                        <a className={styles.navItem}>{props.t["Topics"]}</a>
                     </li>
                 </ul>
                 <ul className={styles.navItems}>
@@ -44,10 +55,37 @@ export default function Header(props) {
                         </a>
                     </li>
                     <li>
-                        <a className={styles.navItem} href={getOppositeLangHomeUrl()}>{props.t.getOppositeLocale.toUpperCase()}</a>
+                        <Link href={getOppositeLangHomeUrl()}>
+                            <a className={styles.navItem}>{props.t.getOppositeLocale.toUpperCase()}</a>
+                        </Link>
                     </li>
                 </ul>
             </div>
+            { isMenuOpen ? 
+                <div className={styles.mobileMenu}>
+                    <div className={styles.closeButton} onClick={toggleMenu}>
+                        <IcomoonReact iconSet={iconSet} size={10} icon="close"/>
+                    </div>
+                    <ul className={styles.navItems}>
+                        {getNavItems().map(navItem => (
+                            <li key={navItem.url}>
+                                <Link href={navItem.url}>
+                                    <a className={styles.navItem}>{navItem.label}</a>
+                                </Link>
+                            </li>
+                        ))}
+                        <li>
+                            <a className={styles.navItem}>{props.t["Topics"]}</a>
+                        </li>
+                        <li>
+                            <Link href={getOppositeLangHomeUrl()}>
+                                <a className={styles.navItem}>{props.t.getOppositeLocale.toUpperCase()}</a>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            : undefined}
+            
         </div>
     );
 }
