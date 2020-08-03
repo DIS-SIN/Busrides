a4f87f2e4834d31628c948a4ff01fbb26d099c83
Jordan Morrison <32398038+Jordan-Morrison@users.noreply.github.com>
2020-08-03 15:18:18 -0400

    Updated styling for the search results page

--- components/templates/SearchResults.js
+++ components/templates/SearchResults.js
@@ -17,31 +17,33 @@ export default function SearchResults(props) {
     return (
         <div>
             <Header t={props.t} settings={props.settings}/>
-            <FullSearchBar t={props.t}/>
-            <div className={styles.resultsContainer}>
-                <div className={styles.mainColumn}>
-                    <div className={styles.titleContainer}>
-                        <h2 className={styles.title}>{props.t["Episodes"]}</h2>
-                        <p className={styles.searchResultsInfo}>{props.t["Searched"]}: {props.searchTerm}, {props.t["got"]} {searchResults.total} {props.t["results"]}</p>
-                        <div className={styles.viewPrefContainer}>
-                            {props.t["View"]}
-                            <a onClick={() => setUseCompactView(false)}>
-                                <IcomoonReact className={styles.spacer} iconSet={iconSet} size={20} color={!useCompactView ? "#CE3F3F" : undefined} icon="grid-view"/>
-                            </a>
-                            <a onClick={() => setUseCompactView(true)}>
-                                <IcomoonReact iconSet={iconSet} size={20} color={useCompactView ? "#CE3F3F" : undefined} icon="list-view"/>
-                            </a>
+            <div className={styles.resultsPage}>
+                <FullSearchBar t={props.t}/>
+                <div className={styles.resultsContainer}>
+                    <div className={styles.mainColumn}>
+                        <div className={styles.titleContainer}>
+                            <h2 className={styles.title}>{props.t["Episodes"]}</h2>
+                            <p className={styles.searchResultsInfo}>{props.t["Searched"]}: {props.searchTerm}, {props.t["got"]} {searchResults.total} {props.t["results"]}</p>
+                            <div className={styles.viewPrefContainer}>
+                                {props.t["View"]}
+                                <a onClick={() => setUseCompactView(false)}>
+                                    <IcomoonReact className={styles.spacer} iconSet={iconSet} size={20} color={!useCompactView ? "#CE3F3F" : undefined} icon="grid-view"/>
+                                </a>
+                                <a onClick={() => setUseCompactView(true)}>
+                                    <IcomoonReact iconSet={iconSet} size={20} color={useCompactView ? "#CE3F3F" : undefined} icon="list-view"/>
+                                </a>
+                            </div>
                         </div>
+                        <EpisodeList t={props.t} posts={searchResults.posts} useCompactView={useCompactView} searchMeta={{
+                            total: searchResults.total,
+                            searchTerm: searchResults.searchTerm,
+                            sortBy: searchResults.sortBy
+                        }}/>
                     </div>
-                    <EpisodeList t={props.t} posts={searchResults.posts} useCompactView={useCompactView} searchMeta={{
-                        total: searchResults.total,
-                        searchTerm: searchResults.searchTerm,
-                        sortBy: searchResults.sortBy
-                    }}/>
-                </div>
-                <div className={styles.sidePanel}>
-                    <SortOptions t={props.t} searchTerm={searchResults.searchTerm} setSearchResults={setSearchResults}/>
-                    <TagsList t={props.t} posts={searchResults.posts}/>
+                    <div className={styles.sidePanel}>
+                        <SortOptions t={props.t} searchTerm={searchResults.searchTerm} setSearchResults={setSearchResults}/>
+                        <TagsList t={props.t} posts={searchResults.posts}/>
+                    </div>
                 </div>
             </div>
             <Footer t={props.t}/>
