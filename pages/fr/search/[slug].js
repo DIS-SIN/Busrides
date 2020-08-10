fcdc96e8e7ddd3c0c88636aa580091f69b91a325
Jordan Morrison <32398038+Jordan-Morrison@users.noreply.github.com>
2020-08-10 11:17:52 -0400

    French Search Update
    Updated the French Search to match the last commit to the English results page

--- pages/fr/search/[slug].js
+++ pages/fr/search/[slug].js
@@ -1,4 +1,8 @@
+import { useEffect, useState } from 'react';
+import Bowser from "bowser";
+import { useWindowWidth } from '@react-hook/window-size';
 import SearchResults from '../../../components/templates/SearchResults';
+import MobileSearchResults from '../../../components/templates/MobileSearchResults';
 import { getTags, getSettings } from '../../../Ghost-API/contentAPI';
 import { getSearchResults } from '../../../Ghost-API/searchAPI';
 import dictionary from '../../../locales/fr';
@@ -6,16 +10,32 @@ import ErrorPage from '../../_error';
 
 export default function Search(props) {
 
+    const SearchResultsPage = <SearchResults t={dictionary} settings={props.settings} searchTerm={props.searchTerm} searchResults={props.searchResults}/>;
+    const MobileSearchResultsPage = <MobileSearchResults t={dictionary} settings={props.settings} searchTerm={props.searchTerm} searchResults={props.searchResults}/>;
+
+    const [resultsPage, setResultsPage] = useState(props.platform === "desktop" ? SearchResultsPage : MobileSearchResultsPage);
+    const width = useWindowWidth();
+
+    useEffect(() => {
+        if (width <= 800 && resultsPage != MobileSearchResultsPage){
+            setResultsPage(MobileSearchResultsPage);
+        }
+        else if (width > 800 && resultsPage != SearchResultsPage){
+            setResultsPage(SearchResultsPage);
+        }
+    },[width]);
+
     if (props.error){
         return <ErrorPage errorCode={404} />
     }
 
-    return (
-        <SearchResults t={dictionary} settings={props.settings} searchTerm={props.searchTerm} searchResults={props.searchResults}/>
-    );
+    return resultsPage;
+    
 }
 
-Search.getInitialProps = async function({query, res}) {
+Search.getInitialProps = async function({query, req}) {
+
+    let platform = Bowser.parse(req.headers["user-agent"]).platform.type;
 
     let searchTerm = decodeURI(query.slug);
 
@@ -25,6 +45,7 @@ Search.getInitialProps = async function({query, res}) {
     let tags = await getTags(dictionary.getTopicSlugs);
 
 	return {
+        platform,
         searchTerm,
         searchResults,
         settings,
