11d76ac168270cdc6e31a74ea51dc4b1db30539b
Jordan Morrison <32398038+Jordan-Morrison@users.noreply.github.com>
2020-08-11 13:55:15 -0400

    Added a progress loading indicator
    Because the site is now an SPA we don't get the browers' loading indicator between pages. This commit is adding a custom loader when the page is changing.

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,15 @@
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 import "../components/stylesheets/styles.css";
+
+// Hide the default spinner to just use the progress bar
+NProgress.configure({ showSpinner: false });
+
+// Add the event handlers so the progress bar works with next router
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
