9fe76735e397a171ac180d8c944cff66b7567d1c
Jordan Morrison <32398038+Jordan-Morrison@users.noreply.github.com>
2020-08-21 16:47:56 -0400

    Browser Warning Test for #10
    This should add a browser warning for users on IE. Going to test in pre-production.

--- openCall/components/BrowserWarning.js
+++ openCall/components/BrowserWarning.js
@@ -1,74 +1,61 @@
 import React from 'react';
 import Bowser from "bowser";
-import { GCFooter } from 'gc-tortilla';
-import { cp_t } from '../../helpers/commonProps';
-import Header from './Header';
-import gocFooterLogo from '../images/gocFooterLogo.png';
-import edgeIcon from '../images/browsers/edge.png';
-import safariIcon from '../images/browsers/safari.png';
-import chromeIcon from '../images/browsers/chrome.png';
-import firefoxIcon from '../images/browsers/firefox.png';
-import operaIcon from '../images/browsers/opera.png';
-
-import './BrowserWarning.css';
+import PropTypes from 'prop-types';
+import Header from '../../components/organisms/Header';
+import Footer from '../../components/organisms/Footer';
+import dictionaryEn from '../../locales/en';
+import dictionaryFr from '../../locales/fr';
+import styles from './BrowserWarning.module.css';
 
 export default function BrowserWarning(props) {
 
     const browser = Bowser.getParser(window.navigator.userAgent);
 
+    const t = props.locale === "fr" ? dictionaryFr : dictionaryEn;
+
     return (
-        <div className="browserWarning">
-            <Header t={props.t}/>
-            <div className="warningContainer">
+        <div className={styles.browserWarning}>
+            <Header t={t} settings={props.settings}/>
+            <div className={styles.warningContainer}>
                 <div>
-                    <p className="uhOh">Uh oh!</p>
-                    <h1>{browser.getBrowserName()} {props.t.browserWarningTitle}</h1>
-                    <p>{props.t.browserWarningMessage}</p>
-                    <div className="browserList">
-                        <div className="browser">
-                            <img src={edgeIcon} alt="Microsoft Edge"/>
+                    <p className={styles.uhOh}>Uh oh!</p>
+                    <h1>{browser.getBrowserName()} {t.browserWarningTitle}</h1>
+                    <p>{t.browserWarningMessage}</p>
+                    <div className={styles.browserList}>
+                        <div className={styles.browser}>
+                            <img src="/images/browsers/edge.png" alt="Microsoft Edge"/>
                             Edge
                         </div>
-                        <div className="browser">
-                            <img src={safariIcon} alt="Safari"/>
+                        <div className={styles.browser}>
+                            <img src="/images/browsers/safari.png" alt="Safari"/>
                             Safari
                         </div>
-                        <div className="browser">
-                            <img src={chromeIcon} alt="Google Chrome"/>
+                        <div className={styles.browser}>
+                            <img src="/images/browsers/chrome.png" alt="Google Chrome"/>
                             Chrome
                         </div>
-                        <div className="browser">
-                            <img src={firefoxIcon} alt="Mozilla Firefox"/>
+                        <div className={styles.browser}>
+                            <img src="/images/browsers/firefox.png" alt="Mozilla Firefox"/>
                             Firefox
                         </div>
-                        <div className="browser">
-                            <img src={operaIcon} alt="Opera"/>
+                        <div className={styles.browser}>
+                            <img src="/images/browsers/opera.png" alt="Opera"/>
                             Opera
                         </div>
                     </div>
                 </div>
             </div>
-            <GCFooter
-                theme={"light"}
-                FIP={{
-                    image: gocFooterLogo,
-                    altText: "Symbol of the Government of Canada / Symbole du gouvernement du Canada"
-                }}
-                links={[
-                    {
-                        url: props.t.getPrivacyLink,
-                        label: props.t["Privacy"]
-                    },
-                    {
-                        url: "https://icons8.com",
-                        label: props.t["Icons"]
-                    }
-                ]}
-            />
+            <Footer t={t}/>
         </div>
     );
 }
 
 BrowserWarning.propTypes = {
-    t: cp_t.isRequired
+    locale: PropTypes.oneOfType([
+        PropTypes.oneOf([
+            "en",
+            "fr"
+        ]),
+        PropTypes.string
+    ]).isRequired
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
+import { useEffect, useState } from 'react';
 import Router from 'next/router';
+import Bowser from 'bowser';
 import NProgress from 'nprogress';
 import 'nprogress/nprogress.css';
+import BrowserWarning from '../openCall/components/BrowserWarning';
 import "../components/stylesheets/styles.css";
 
 // Hide the default spinner to just use the progress bar
@@ -13,5 +16,15 @@ Router.events.on('routeChangeError', () => NProgress.done());
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+    const [goodBrowser, setGoodBrowser] = useState(true);
+
+    useEffect(() => {
+        let browser = Bowser.getParser(window.navigator.userAgent);
+        if (browser.getBrowserName() === "Internet Explorer"){
+            setGoodBrowser(false);
+        }
+    },[]);
+
+    return goodBrowser ? <Component {...pageProps} /> : <BrowserWarning {...pageProps}/>;
 }
