a843d3dc709ab35751a3ebc4a650ecc66400229a
Jordan Morrison <32398038+Jordan-Morrison@users.noreply.github.com>
2020-11-30 16:49:31 -0500

    Ghost Pro API
    Now using the API through Ghost Pro hosting

--- Ghost-API/contentAPI.js
+++ Ghost-API/contentAPI.js
@@ -2,9 +2,9 @@ import GhostContentAPI from "@tryghost/content-api";
 
 // Create API instance with site credentials
 const api = new GhostContentAPI({
-    url: 'https://busrides-trajetsenbus.ca',
-    key: 'ac6e3e888da0c5bda4e1ed9e60',
-    version: "v2"
+    url: 'https://busrides.ghost.io',
+    key: 'dbc5b6eeac0de80a555116ddf8',
+    version: "v3"
 });
 
 export async function getPosts(options) {
