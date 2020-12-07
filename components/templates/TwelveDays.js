ed8deafb013f8d18308578ca574e99caa4f1ece4
Jordan Morrison <32398038+Jordan-Morrison@users.noreply.github.com>
2020-12-07 17:03:45 -0500

    #32 Fixes + French version
    Added the ability to choose a custom image
    
    Fixed the title, description + meta tags from being static
    
    Added the French version

--- components/templates/TwelveDays.js
+++ components/templates/TwelveDays.js
@@ -1,8 +1,6 @@
 import React, {useState} from 'react';
 import MetaTags from '../molecules/MetaTags';
 import Header from '../organisms/Header';
-import Hero from '../organisms/Hero';
-import PostContent from '../organisms/PostContent';
 import Footer from '../organisms/Footer';
 import NoPeeking from '../12-days/NoPeeking';
 import Flap from '../12-days/Flap';
@@ -21,16 +19,13 @@ export default function TwelveDays(props) {
 
     return (
         <React.Fragment>
-            <MetaTags title={"Twelve Days"} description={"Description"} image={"https://raw.githubusercontent.com/DIS-SIN/12-Days-of-Data/master/src/images/bg.jpg"} url={'http://localhost:3000/en/12-days'}/>
+            <MetaTags title={props.data.meta.title} description={props.data.meta.description} image={props.data.meta.image} url={props.data.meta.url}/>
             <Header t={props.t} settings={props.settings}/>
-            {/* <Hero backgroundImage={"https://www.ghd.com/en/about-us/resources/News-insights/Insights/digital/snowy-mountains.jpg"}>
-                <h1>{"Twelve Days"}</h1>
-            </Hero> */}
             <div className={styles.header}>
-                <h1>Welcome to Busrides’ 12 days of data!</h1>
-                <p>The 12 most interesting data facts and resources sit behind the calendar doors. A new door will be available to open every day until December 20th.</p>
+                <h1>{props.data.title}</h1>
+                <p>{props.data.description}</p>
             </div>
-            <div className={styles.twelveDays} style={{background: `url("https://raw.githubusercontent.com/DIS-SIN/12-Days-of-Data/master/src/images/bg.jpg")`}}>
+            <div className={styles.twelveDays} style={{background: `url(${props.data.image})`, backgroundSize: "cover"}}>
                 {error.exists ? <NoPeeking error={error} setError={setError}/> : null}
                 <div className={styles.calendar}>
                     {days.map(day => <Flap data={props.data} key={day} day={day} openContent={openContent} setError={setError} setDayToOpen={setDayToOpen}/>)}
