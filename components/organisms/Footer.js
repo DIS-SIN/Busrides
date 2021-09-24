2ea2312da54c5c11738e4816ec0012212b2e6e6a
Keven Rousseau <keven.rousseau@csps-efpc.gc.ca>
2021-09-24 10:11:39 -0400

    Integrated the GCWeb theme Localized the header and footer Fixed styling conflicts with app design Home page fixed Deactivated the dark theme for now

--- components/organisms/Footer.js
+++ components/organisms/Footer.js
@@ -5,20 +5,56 @@ import { cp_t } from '../../helpers/commonProps';
 import styles from '../stylesheets/Footer.module.css';
 
 export default function Footer(props) {
-
+console.log(props.t);
     return (
-        <div className={styles.footer}>
-            <div className={styles.footerContent}>
-                <p className={styles.row}>
-                    <span>{props.t["Busrides"]} © 2021&nbsp;&nbsp;•&nbsp;&nbsp;</span>
-                    <a href={props.t.getAboutDALink} target="_blank" rel="noopener">{props.t["What is the Digital Academy?"]}</a>
-                </p>
-                <a className={styles.row} href={props.t.getDATwitter} aria-label={props.t["Digital Academy Twitter"]} target="_blank" rel="noopener">
-                    <IcomoonReact iconSet={iconSet} size={16} icon="twitter"/>
-                </a>
-                <a className={styles.row} href="https://da-an.us3.list-manage.com/subscribe?u=9e5810d743bf898c302d3c312&id=bbc8bab3e7" target="_blank" rel="noopener">{props.t["Subscribe to our newsletter"]}</a>
+        <footer id="wb-info">
+            <div className={styles.footer}>
+                <div className={styles.footerContent}>
+                    <p className={styles.row}>
+                        <span>{props.t["Busrides"]} © 2021&nbsp;&nbsp;•&nbsp;&nbsp;</span>
+                        <a href={props.t.getAboutDALink} target="_blank" rel="noopener">{props.t["What is the Digital Academy?"]}</a>
+                    </p>
+                    <a className={styles.row} href={props.t.getDATwitter} aria-label={props.t["Digital Academy Twitter"]} target="_blank" rel="noopener">
+                        <IcomoonReact iconSet={iconSet} size={16} icon="twitter"/>
+                    </a>
+                    <a className={styles.row} href="https://da-an.us3.list-manage.com/subscribe?u=9e5810d743bf898c302d3c312&id=bbc8bab3e7" target="_blank" rel="noopener">{props.t["Subscribe to our newsletter"]}</a>
+                </div>
             </div>
-        </div>
+            <div className="landscape">
+                <nav className="container wb-navcurr">
+                    <h2 className="wb-inv">{props.t["about_government"]}</h2>
+                    <ul className="list-unstyled colcount-sm-2 colcount-md-3">
+                        <li><a href={props.t["Contact us URL"]}>{props.t["Contact us text"]}</a></li>
+                        <li><a href={props.t["Departments and agencies URL"]}>{props.t["Departments and agencies text"]}</a></li>
+                        <li><a href={props.t["Public service and military URL"]}>{props.t["Public service and military text"]}</a></li>
+                        <li><a href={props.t["News URL"]}>{props.t["News text"]}</a></li>
+                        <li><a href={props.t["Treaties laws and regulations URL"]}>{props.t["Treaties laws and regulations text"]}</a></li>
+                        <li><a href={props.t["Government wide reporting URL"]}>{props.t["Government wide reporting text"]}</a></li>
+                        <li><a href={props.t["Prime minister URL"]}>{props.t["Prime minister text"]}</a></li>
+                        <li><a href={props.t["How government works URL"]}>{props.t["How government works text"]}</a></li>
+                        <li><a href={props.t["Open government URL"]}>{props.t["Open government text"]}</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div className="brand">
+                <div className="container">
+                    <div className="row">
+                        <nav className="col-md-9 col-lg-10 ftr-urlt-lnk">
+                            <h2 className="wb-inv">{props.t["about_this_site"]}</h2>
+                            <ul>
+                                <li><a href={props.t["Social media URL"]}>{props.t["Social media text"]}</a></li>
+                                <li><a href={props.t["Mobile applications URL"]}>{props.t["Mobile applications text"]}</a></li>
+                                <li><a href={props.t["About canada.ca URL"]}>{props.t["About canada.ca text"]}</a></li>
+                                <li><a href={props.t["Terms and conditions URL"]}>{props.t["Terms and conditions text"]}</a></li>
+                                <li><a href={props.t["Privacy URL"]}>{props.t["Privacy text"]}</a></li>
+                            </ul>
+                        </nav>
+                        <div className="col-xs-6 visible-sm visible-xs tofpg"> <a href="#wb-cont">{props.t["top_of_page"]} <span className="glyphicon glyphicon-chevron-up"></span></a> </div>
+                        <div className="col-xs-6 col-md-3 col-lg-2 text-right"> <img src="/theme/GCWeb/assets/wmms-blk.svg" alt={props.t["symbol_of_the_government_of_canada"]} /> </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
     );
 }
 
--- locales/fr.js
+++ locales/fr.js
@@ -49,7 +49,61 @@ const fr = {
     "learningPathsSlogan": "L'apprentissage dont vous avez besoin, quand et comme vous l'avez besoin.",
     "Videos": "Démos d'outils de données du GC",
     "VideosSlogan": "La bibliothèque de démos d’outils de données du GC est une collection de vidéos de projets de données mettant en évidence les innovations de la communauté des données du GC qui démontrent le pouvoir des données pour informer de meilleures politiques, programmes et opérations.",
-    "Grayscale": "Gris"
+    "Grayscale": "Gris",
+
+    // GCTheme locales
+
+    // Header
+    "Language Title"          : "Sélection de la langue",
+    "Logo Title"              : "Gouvernment du Canada / <span lang='en'>Governement of Canada</span>",
+    "Search"                  : "Recherche",
+    "Search Placeholder"      : "Rechercher dans Canada.ca",
+    "Menu"                    : "Menu",
+    "Menu Title"              : "Menu <span class='wb-inv'> principal</span> <span class='expicon glyphicon glyphicon-chevron-down'></span>",
+    "You are here"            : "Vous êtes ici :",
+    "Skip to main content"    : "Passer au contenu principal",
+    "Skip to about government": "Passer à « Au sujet du gouvernement »",
+    "Search Action"           : "https://www.canada.ca/fr/ecole-fonction-publique/rechercher.html",
+    // Breadcrumb
+    "Canada.ca"                      : "Canada.ca",
+    "Canada School of Public Service": "École de la fonction publique du Canada",
+    "Learning catalogue"             : "Catalogue d'apprentissage",
+    "Learning tools"                 : "Outils d'apprentissage",
+
+    'about_government'                  : 'Au sujet du gouvernement',
+    'about_this_site'                   : 'Au sujet de ce site',
+    'top_of_page'                       : 'Haut de la page',
+    'symbol_of_the_government_of_canada': 'Symbole du gouvernement du Canada',
+
+    // Footer
+    'Contact us text': 'Contactez-nous',
+    'Contact us URL': 'https://www.canada.ca/fr/contact.html',
+    'Departments and agencies text': 'Ministères et organismes',
+    'Departments and agencies URL': 'https://www.canada.ca/fr/gouvernement/min.html',
+    'Public service and military text': 'Fonction publique et force militaire',
+    'Public service and military URL': 'https://www.canada.ca/fr/gouvernement/fonctionpublique.html',
+    'News text': 'Nouvelles',
+    'News URL': 'https://www.canada.ca/fr/nouvelles.html',
+    'Treaties laws and regulations text': 'Traités, lois et règlements',
+    'Treaties laws and regulations URL': 'https://www.canada.ca/fr/gouvernement/systeme/lois.html',
+    'Government wide reporting text': 'Rapports à l\'échelle du gouvernement',
+    'Government wide reporting URL': 'https://www.canada.ca/fr/transparence/rapports.html',
+    'Prime minister text': 'Premier ministre',
+    'Prime minister URL': 'http://pm.gc.ca/fra',
+    'How government works text': 'Comment le gouvernement fonctionne',
+    'How government works URL': 'https://www.canada.ca/fr/gouvernement/systeme.html',
+    'Open government text': 'Gouvernement ouvert',
+    'Open government URL': 'http://ouvert.canada.ca/',
+    'Social media text': 'Médias sociaux',
+    'Social media URL': 'https://www.canada.ca/fr/sociaux.html',
+    'Mobile applications text': 'Applications mobiles',
+    'Mobile applications URL': 'https://www.canada.ca/fr/mobile.html',
+    'About canada.ca text': 'À propos de Canada.ca',
+    'About canada.ca URL': 'https://www.canada.ca/fr/gouvernement/a-propos.html',
+    'Terms and conditions text': 'Avis',
+    'Terms and conditions URL': 'https://www.canada.ca/fr/transparence/avis.html',
+    'Privacy text': 'Confidentialité',
+    'Privacy URL': 'https://www.canada.ca/fr/transparence/confidentialite.html',
 }
 
 export default fr;
