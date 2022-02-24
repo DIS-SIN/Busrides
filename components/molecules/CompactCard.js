63b9d96587937920731a9f30e658de81269865ba
Stef Jomphe <41083520+stef-barburou@users.noreply.github.com>
2022-02-24 13:27:25 -0500

    also on compact card

--- components/molecules/CompactCard.js
+++ components/molecules/CompactCard.js
@@ -39,7 +39,7 @@ export default function CompactCard(props) {
                         <p className={styles.timeStamps}>{getTimeSincePublished()}<span>•</span>{getReadingTime()}</p>
                     </div>
                     <div className={styles.bottomContent}>
-                        <h2 className={styles.title}><a href="#">{props.post.title}</a></h2>
+                        <h2 className={styles.title}><a href={props.post.url} className={styles.cardtitle}>{props.post.title}</a></h2>
                         <p className={styles.description}>{props.post.excerpt}</p>
                     </div>
                 </div>
